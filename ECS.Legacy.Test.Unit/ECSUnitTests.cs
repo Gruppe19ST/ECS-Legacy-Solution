6faef71e3b07d0ba6813e06185dedca74c27d48a
Anni <201509504@post.au.dk>
2018-02-19 16:42:59 +0100

    Leg med Stub og unit tests (uden større held)

--- ECS.Legacy.Test.Unit/ECSUnitTests.cs
+++ ECS.Legacy.Test.Unit/ECSUnitTests.cs
@@ -11,16 +11,65 @@ namespace ECS.Legacy.Test.Unit
     public class ECSUnitTests
     {
         private ECS _uutEcs;
-        private int threshold;
-        private ITempSensor _tempsensor;
-        private IHeater _heater;
+        private int _threshold;
         
 
         [SetUp]
         public void Setup()
         {
-            _uutEcs = new ECS(threshold,_tempsensor,_heater);
+            _threshold = 20;
+            _uutEcs = new ECS(_threshold, new StubTempSensor(), new StubHeater());
         }
 
+        [Test]
+        public void GetCurTemp_SetToReturn0_Returns0()
+        {
+            Assert.AreEqual(_uutEcs.GetCurTemp(),0);
+        }
+
+        [Test]
+        public void GetThreshold_ThresholdSetTo20_Returns20()
+        {
+            _uutEcs.SetThreshold(20);
+            Assert.AreEqual(_uutEcs.GetThreshold(), 20);
+        }
+        //[Test]
+        //public void Regulate_TempOverThreshold_HeaterOff()
+        //{
+           
+        //}
+
     }
+
+    internal class StubTempSensor : ITempSensor
+    {
+        public int GetTemp()
+        {
+            return 25;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+
+    internal class StubHeater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+
 }
