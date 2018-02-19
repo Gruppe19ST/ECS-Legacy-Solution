9388af4c6e15d00d7069ba7434aa61a065fbc138
Anni <201509504@post.au.dk>
2018-02-19 15:46:46 +0100

    Added NUnit package Worked on Test-project

---
+++ ECS.Legacy.Test.Unit/ECSUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy.Test.Unit
+{
+    [TestFixture]
+    public class ECSUnitTests
+    {
+        private ECS _uutEcs;
+        private int threshold;
+        private ITempSensor _tempsensor;
+        private IHeater _heater;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            _uutEcs = new ECS(threshold,_tempsensor,_heater);
+        }
+
+    }
+}
